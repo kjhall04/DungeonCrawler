aa3af63f083e452ad36e959b8d00680cf015bc82
kjhall04 <hallkj04@gmail.com>
2025-04-09 13:22:20 -0500

    changes for vercel

--- api/backend/app/__init__.py
+++ api/backend/app/__init__.py
@@ -7,7 +7,11 @@ import os
 def create_app():
     """Create and configure the flask app."""
     load_dotenv()
-    app = Flask(__name__, template_folder='../../frontend/templates', static_folder='../../frontend/static')
+    app = Flask(
+        __name__, 
+        template_folder='../frontend/templates', 
+        static_folder='../frontend/static'
+    )
     app.secret_key = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')
 
     # Register blueprints
